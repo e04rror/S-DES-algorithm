4dbacb94065d4f2f08d23dcf7ab6ca9c1f370d8f
e04rror <vaniaua9@gmail.com>
2024-06-01 22:26:16 +0300

    Modified main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <thread>
+#include <cstdlib>
 
-//i want to create function that use threads to encrypt and decrypt at the same time 
+void encrypt() {
+  system("./encrypt");
+}
+
+void decrypt() {
+  system("./decrypt");
+}
 
 int main(){
+  std::thread t1(encrypt);
+  std::thread t2(decrypt);
+
+  
+  t1.join();
+  t2.join();
+
   return 0;
 }
