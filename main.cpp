289f709c30380a40e78390e225e2179f0d07d7c6
e04rror <vaniaua9@gmail.com>
2024-06-02 11:17:51 +0300

    Now, the decryption file will wait until the encryption file completes

--- main.cpp
+++ main.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 #include <thread>
 #include <cstdlib>
+#include <future>
 
-void encrypt() {
+void encrypt(std::promise<void> &obj) {
   system("./encrypt");
+
+  std::cout<<"THIS IS DECRYPT!"<<std::endl;
+  obj.set_value();
 }
 
-void decrypt() {
+void decrypt(std::future<void> &obj) {
+  obj.wait();
   system("./decrypt");
 }
 
 int main(){
-  std::thread t1(encrypt);
-  std::thread t2(decrypt);
+  std::promise<void> forEncryptionEnd;
+  std::future<void> waitForEncrypt = forEncryptionEnd.get_future();
+
+  std::thread t1(encrypt, std::ref(forEncryptionEnd));
+  std::thread t2(decrypt, std::ref(waitForEncrypt));
 
   
   t1.join();
